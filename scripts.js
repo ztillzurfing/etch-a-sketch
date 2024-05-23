32c8371765f0e6f15fde301c8be25810d0a827a2
ztillzurfing <94315453+ztillzurfing@users.noreply.github.com>
2024-05-23 21:39:51 +0100

    link style.css

--- scripts.js
+++ scripts.js
@@ -4,8 +4,10 @@ function createDiv (num) {
     for (let i = 0; i <num; i++ ) {
         const content = document.createElement("div")
         content.setAttribute("class", "content")
+        content.textContent("hello")
         container.appendChild(content);
+        
     }
 }
 
-createDiv(16);
+createDiv(30);
