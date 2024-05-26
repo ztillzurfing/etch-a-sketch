fa2a8f980182e11096aa7e982c038b4365d623aa
ztillzurfing <94315453+ztillzurfing@users.noreply.github.com>
2024-05-26 21:24:50 +0100

    Add squareNum,removeDiv functions and start newDiv

--- scripts.js
+++ scripts.js
@@ -4,12 +4,52 @@ const container = document.querySelector("#container")
 function createDiv (num) {
     for (let i = 0; i < num; i++ ) {
         const div = document.createElement("div");
-        div.classList.add("content"); 
+        div.classList.add("content");
+        div.addEventListener("mouseover", () =>{
+            div.style.backgroundColor = "blue"
+        })
         container.appendChild(div);
         
     }
 }
 
-createDiv(256);
-console.log("hello")
+// function hover() {
+//     const hover = document.querySelectorAll("div")
+//     hover.addEventListener("mouseover", () => {
+//         hover.classList.add("hover");
+//         div.appendChild(hover);
+//     })
+// }
 
+
+createDiv(256);
+// hover();
+
+
+function newGrid (number) {
+
+    removeDiv();
+    
+    divNum = squareNum(number)
+    for (let i = 0; i < divNum; i++ ) {
+        
+        const div = document.createElement("div");
+        div.classList.add("content");
+        div.addEventListener("mouseover", () =>{
+            div.style.backgroundColor = "blue"
+        })
+        container.appendChild(div);
+        
+    }
+}
+
+function squareNum(number) {
+    return number ** 2;
+}
+
+function removeDiv () {
+    const oldDiv = document.getElementById("container");
+    while (oldDiv.firstChild) {
+        oldDiv.removeChild(oldDiv.lastChild);
+    }
+}
